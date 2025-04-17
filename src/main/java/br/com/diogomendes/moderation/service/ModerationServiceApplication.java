2c8d4ec7e97d28cda37836fb3d380eec6fdbf36f
didamendes <didamendes@hotmail.com>
2025-04-16 21:35:36 -0300

    Começo do projetos.

---
+++ src/main/java/br/com/diogomendes/moderation/service/ModerationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogomendes.moderation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationServiceApplication.class, args);
+	}
+
+}
